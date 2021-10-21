ed093f560efff92a6f2251a310400900d9556c5d
sowmya <sowmya@10.0.0.156>
2021-10-21 16:31:44 -0700

    first commit

---
+++ sampleproject/src/sampleproject/TestWorld.java
@@ -0,0 +1,14 @@
+package sampleproject;
+
+public class TestWorld {
+
+	public TestWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
